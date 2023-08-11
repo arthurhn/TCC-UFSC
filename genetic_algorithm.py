ba3f94e606159aeda1c13edf0ad71f96eef49085
LuanAlflen <luan.alflen4@gmail.com>
2023-08-11 13:10:34 -0300

    Add files via upload

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,17 +1,63 @@
-#Código semelhante ao 2.1.0, porém para testes de influencia com variação de comprimento e largura, 
-#porém no foi feito o crossover para a variação de comprimento e largura (talvez melhore com isso?
-#Pois até então a variação de comprimento e largura não se mostrou muito eficaz em termos de melhora 
-#de convergencia)
-#(16/07/2023)
-
+#fazer ainda o select pressure aqui
 
 import time
 import random
 import numpy as np
 import os
 import sys
+import matplotlib.pyplot as plt
 from v1_1_fisics_dumper import *
 
+limit_generation_ = 1000
+generation_size_ = 250
+chart_on = True
+if(chart_on):
+    plt.ion()
+class DynamicUpdate():
+    #Suppose we know the x range
+    min_x = 0
+    max_x = limit_generation_
+
+    def on_launch(self):
+        #Set up plot
+        self.figure, self.ax = plt.subplots(2)
+        self.figure.suptitle('Flexible Lenght/Width - Local Search')
+        self.lines, = self.ax[0].plot([],[], '-')
+        self.lines1, = self.ax[1].plot([],[], '-')
+        #Autoscale on unknown axis and known lims on the other
+        self.ax[0].set_autoscaley_on(True)
+        self.ax[0].set_xlim(self.min_x, self.max_x)
+        self.ax[1].set_autoscaley_on(True)
+        self.ax[1].set_xlim(self.min_x, self.max_x)
+        #Other stuff
+        self.ax[0].set_ylabel('Best Accuracy')
+        self.ax[1].set_xlabel('Generation')
+        self.ax[1].set_ylabel('Select Pressure')
+        self.ax[0].grid()
+        self.ax[1].grid()
+
+    def on_running(self, xdata, ydata1, ydata2):
+        #Update data (with the new _and_ the old points)
+        self.lines, = self.ax[0].plot(xdata,ydata1, 'r-')
+        self.lines1, = self.ax[1].plot(xdata,ydata2, 'b-')
+        # self.lines.set_ydata(new_ydata)
+        #Need both of these in order to rescale
+        self.ax[0].relim()
+        self.ax[1].relim()
+        self.ax[1].autoscale_view()
+        self.ax[0].autoscale_view()
+        #We need to draw *and* flush
+        self.figure.canvas.draw()
+        self.figure.canvas.flush_events()
+       
+undone = False
+
+if(chart_on):
+    d = DynamicUpdate()
+    d.on_launch()
+xdata = []
+ydata_sp = []
+ydata_ac = []
 
 #inicial parameters
 error_ = [0.1, 0.1, 0.1, 0.1, 0.1]
@@ -20,6 +66,20 @@ rigidez_lenght_min = 4/1000
 rigidez_lenght_max = 15/1000
 aco_lenght_min = 1/1000
 aco_lenght_max = 15/1000
+
+#other parameters
+
+#standards frequency targets
+frequency_target = [1774,1524,1452,1025,354]
+# frequency_target = [1774,1524,1452,1025,675]
+# frequency_target = [2000, 1500, 1000, 700, 200]
+# frequency_target = [6000, 3300, 1100, 400, 200]
+
+#real materials thickness (mm)
+rigidez_options_mm = [2, 3, 4]
+aco_options_mm = [3, 3.3, 3.8, 5]
+comprimento_options = [0.40, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.50, 0.51, 0.52, 0.53, 0.54, 0.55, 0.55, 0.56, 0.57, 0.58, 0.59, 0.60]
+largura_options = [0.045, 0.046, 0.047, 0.048, 0.049, 0.050, 0.051, 0.052, 0.053, 0.054, 0.055]
 
 #getting parameters from executation line
 if(len(sys.argv) >= 11):
@@ -35,18 +95,8 @@ if(len(sys.argv) >= 11):
 av_error = np.sum(error_)/5
 accuracy_target = 1/av_error
 discrete_value = discrete_value_mm/1000
+select_pressure_vector = []
 
-#standards frequency targets
-frequency_target = [1774,1524,1452,1025,354]
-# frequency_target = [1774,1524,1452,1025,675]
-# frequency_target = [2000, 1500, 1000, 700, 200]
-# frequency_target = [6000, 3300, 1100, 400, 200]
-
-#real materials thickness (mm)
-rigidez_options_mm = [1, 2, 3, 4]
-aco_options_mm = [3, 3.3, 3.8, 5]
-comprimento_options = [0.40, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.50, 0.51, 0.52, 0.53, 0.54, 0.55, 0.55, 0.56, 0.57, 0.58, 0.59, 0.60]
-largura_options = [0.045, 0.046, 0.047, 0.048, 0.049, 0.050, 0.051, 0.052, 0.053, 0.054, 0.055]
 
 #auxiliar functions
 def expovariate(len_ranked_rigidez):
@@ -85,6 +135,9 @@ def print_current_best_individual(best_tuple, rankedsolutions, i):
     print(f"  Mass (mm):         {[round(er[2]*1000, 2) for er in best_tuple[2]]}")
     print(f"  Lenght (cm):       {round(best_tuple[5]*100, 1)}")
     print(f"  Width (mm):        {round(best_tuple[6]*1000, 1)}")
+    print(f"  Elasticity (MPa):  {round(Et/(10e5), 2)}")
+
+
     print()
     print(f"=== Gen {i+1} bests solutions ===")
     for j in range(3):
@@ -93,8 +146,36 @@ def print_current_best_individual(best_tuple, rankedsolutions, i):
         total_weight = weight_calculate(rankedsolutions[j][2], rankedsolutions[j][1], 0.50, 0.051)
         # print(f"Error: {round(aux_error, 2)}% and vector_error: {[round(100*er, 2) for er in rankedsolutions[j][3]]}%")
         # print(f"Average Error: {round(aux_error, 2)}% and Weight: {round(total_weight, 2)}kg")
-        print(f"Accuracy: {round(rankedsolutions[j][0], 4)},   Weight: {round(total_weight, 2)}kg,   error per freq: {[round(er*100, 2) for er in rankedsolutions[j][3]]}")
+        print(f"Accuracy: {round(rankedsolutions[j][0], 4)},   Weight: {round(total_weight, 2)}kg.")
         # print(f"Error: {[round(er, 2) for er in rankedsolutions[j][3]]}% and Weight: {round(total_weight, 2)}kg")
+    print()
+    #Select Pressure printing
+    best_accurary, average_accurary = select_pressure(rankedsolutions)
+    g0 = 1/best_accurary
+    g = 1/average_accurary
+    current_select_pressure = g0/g 
+    select_pressure_vector.append(current_select_pressure)
+    print(f"Current Select Pressure: {round(current_select_pressure*100, 2)}%,")
+    print(f"Best error: {round(g0*100, 2)}%, Average error: {round(g*100, 2)}%")
+    print("Mutation Local")
+
+    #----------- Printing chart ---------------------
+    if(chart_on):
+        xdata.append(i)
+        ydata_sp.append(current_select_pressure)
+        ydata_ac.append(rankedsolutions[0][0])
+        if(limit_generation_ < 150):
+            d.ax[0].set_xlim(0, limit_generation_-1)
+            d.ax[1].set_xlim(0, limit_generation_-1)
+        if(i%150 == 0):
+            if(i==150):
+                plt.savefig('parcial_sp_2_2_1.png', dpi=300)
+            d.ax[0].set_xlim(i, i+150)
+            d.ax[1].set_xlim(i, i+150)
+        if(i==limit_generation_-1):
+            d.ax[0].set_xlim(0, limit_generation_-1)
+            d.ax[1].set_xlim(0, limit_generation_-1)
+        d.on_running(xdata, ydata_ac, ydata_sp)
 def print_best_individual(start, end, correct_ans, best_tuple):
     os.system('cls' if os.name == 'nt' else 'clear')
     print()
@@ -117,6 +198,7 @@ def print_best_individual(start, end, correct_ans, best_tuple):
     average_error = average_error/len(correct_ans)
     print(f"Error per frequency:     {[round(err*100, 2) for err in best_tuple[3]] }%")
     print(f"Average error:           {round(average_error*100, 4)}%")
+    print(f"Accuracy:                {round(best_tuple[0], 4)}")
     print()
 
     total_weight = weight_calculate(best_tuple[2], best_tuple[1], 0.50, 0.051)
@@ -132,8 +214,10 @@ def print_best_individual(start, end, correct_ans, best_tuple):
     print(f"Mass thickness(mm):      {[round(r, 2) for r in aco_mm]}")
     print(f"Lenght (cm):             {round(best_tuple[5]*100, 1)}")
     print(f"Width (mm):              {round(best_tuple[6]*1000, 1)}")
+    print(f"Accuracy:                {round(best_tuple[0], 4)}")
+    print(f"Elasticity (MPa):  {round(Et/(10e5), 2)}")
+    print("Mutation Local")
     print()
-
     print(f"Finished in {round(end-start, 2)}s / {round((end-start)/60)}min / {round((end-start)/60/60, 2)}h")
     return
 
@@ -207,6 +291,7 @@ def selection_function(p_rigidez, p_aco, p_comprimento, p_largura, correct_ans, 
     vector_ans = parcial_vector[2]
     best_tuple = (precision, rigidez, aco, vector_errors, vector_ans, p_comprimento[0], p_largura[0])
 
+    unfinished = True
     start = time.time()
     for i in range(limit_generations):
         rankedsolutions = []
@@ -241,22 +326,12 @@ def selection_function(p_rigidez, p_aco, p_comprimento, p_largura, correct_ans, 
 
         #print the best individual info
         print_current_best_individual(best_tuple, rankedsolutions, i)
-        
-        # print(rankedsolutions[0])
-        # if(i == 60):
-        #     sys.exit()
-
 
         #stop code if it find the optimal individual based on requirements
         best_error = rankedsolutions[0][3]
         if  (best_error[0] < error_[0] and best_error[1] < error_[1] and best_error[2] < error_[2] and best_error[3] < error_[3] and best_error[4] < error_[4]):
             best_tuple = rankedsolutions[0]
-            print("Preparing results")
-            # print("Preparing results", end="")
-            time.sleep(1.5)
-            for _ in range(3):
-                print(".")
-                time.sleep(0.6)
+            unfinished = False
             break
 
         crossover_generation = crossover_function(rankedsolutions[:10])
@@ -266,6 +341,9 @@ def selection_function(p_rigidez, p_aco, p_comprimento, p_largura, correct_ans, 
     #print the final solution at end of the limit generation
     end = time.time()
     print_best_individual(start, end, correct_ans, best_tuple)
+    if(unfinished == False):
+        return False
+    return True
 
 #mix thickness between individuals
 def crossover_function(bestsolutions):
@@ -365,6 +443,118 @@ def mutation_function(elements, generation_size):
             i = random.randint(0, len(crossover_rigidez)-1)+len(ranked_rigidez)
         #------------------------------------------------------------------------------------------
         signal = [-1, 1]
+
+        
+        how_is_change = random.randint(0, 3) #0 -> only rigidez will change, 1 -> only aco, 2 -> both, 3 -> none 
+
+        if(how_is_change == 0):
+            #rigidez changed
+            for j in range(len(solutions_rigidez[0])):
+                new_lenght_rigidez = select_thickness(rigidez_options_mm)[0]
+                # new_lenght_rigidez = abs(solutions_rigidez[i][j] + signal[random.randint(0, 1)] * (discrete_value * random.randint(0, 3)))
+                while new_lenght_rigidez < 0.000001:
+                    new_lenght_rigidez = select_thickness(rigidez_options_mm)[0]
+                    # new_lenght_rigidez = abs(solutions_rigidez[i][j] + signal[random.randint(0, 1)] * (discrete_value * random.randint(0, 3)))
+                one_rigidez.append(round(new_lenght_rigidez, 6)) #current number +- 0.001 or 0.002 ....
+            #aco doesnt change
+            one_aco = [solutions_aco[i][j] for j in range(len(solutions_aco[i]))]
+
+        elif(how_is_change == 1):
+            #rigidez doesnt change
+            one_rigidez = [solutions_rigidez[i][j] for j in range(len(solutions_rigidez[i]))]
+            #aco changed
+            for j in range(len(solutions_aco[0])):
+                new_lenght_aco = select_thickness(aco_options_mm)[0]
+                # new_lenght_aco = abs(solutions_aco[i][j] + signal[random.randint(0, 1)] * (discrete_value * random.randint(0, 3)))
+                while new_lenght_aco < 0.000001:
+                    new_lenght_aco = select_thickness(aco_options_mm)[0]
+                    # new_lenght_aco = abs(solutions_aco[i][j] + signal[random.randint(0, 1)] * (discrete_value * random.randint(0, 3)))
+                one_aco.append(round(new_lenght_aco, 6)) #current number +- 0.001 or 0.002 ....
+
+        elif(how_is_change == 2):
+            #both changed
+            for j in range(len(solutions_aco[0])):
+                new_lenght_aco = select_thickness(aco_options_mm)[0]
+                # new_lenght_aco = abs(solutions_aco[i][j] + signal[random.randint(0, 1)] * (discrete_value * random.randint(0, 3)))
+                while new_lenght_aco < 0.000001:
+                    new_lenght_aco = select_thickness(aco_options_mm)[0]
+                    # new_lenght_aco = abs(solutions_aco[i][j] + signal[random.randint(0, 1)] * (discrete_value * random.randint(0, 3)))
+                one_aco.append(round(new_lenght_aco, 6)) #current number +- 0.001 or 0.002 ....
+            for j in range(len(solutions_rigidez[0])):
+                new_lenght_rigidez = select_thickness(rigidez_options_mm)[0]
+                # new_lenght_rigidez = abs(solutions_rigidez[i][j] + signal[random.randint(0, 1)] * (discrete_value * random.randint(0, 3)))
+                while new_lenght_rigidez < 0.000001:
+                    new_lenght_rigidez = select_thickness(rigidez_options_mm)[0]
+                    # new_lenght_rigidez = abs(solutions_rigidez[i][j] + signal[random.randint(0, 1)] * (discrete_value * random.randint(0, 3)))
+                one_rigidez.append(round(new_lenght_rigidez, 6)) #current number +- 0.001 or 0.002 ....
+
+        elif(how_is_change == 3):
+            #both doesnt change
+            one_rigidez = [solutions_rigidez[i][j] for j in range(len(solutions_rigidez[i]))]
+            one_aco = [solutions_aco[i][j] for j in range(len(solutions_aco[i]))]
+      
+        mutation_on_lenght_or_width = [0, 1, 2]
+        choice2 = random.choice(mutation_on_lenght_or_width)
+        if(choice2 == 0):
+            new_lenght.append(random.choice(comprimento_options))
+            new_width.append(random.choice(ranked_width))
+        elif(choice2 == 1):
+            new_width.append(random.choice(largura_options))
+            new_lenght.append(random.choice(ranked_lenght))
+        else:
+            new_width.append(random.choice(largura_options))
+            new_lenght.append(random.choice(comprimento_options))
+
+        new_rigidez.append(one_rigidez)
+        new_aco.append(one_aco)
+    for ri in ranked_rigidez:
+        new_rigidez.append(ri)
+    for ac in ranked_aco:
+        new_aco.append(ac)
+    for le in ranked_lenght:
+        new_lenght.append(le)
+    for wi in ranked_width:
+        new_width.append(wi)
+    return new_rigidez, new_aco, new_lenght, new_width
+
+#old mutation function, not considering any crossover if use this one
+def global_mutation_function(elements, generation_size):
+    ranked_rigidez, ranked_aco, ranked_lenght, ranked_width, crossover_rigidez, crossover_aco = elements
+    new_rigidez = []
+    new_aco = []
+    new_lenght = []
+    new_width = []
+    assert len(ranked_rigidez) == len(ranked_aco), "Vetor p_ridizes e p_aco com tamanhos diferentes, mutation_function"
+    assert len(crossover_rigidez) == len(crossover_aco), "Vetor crossover_rigidez e crossover_aco com tamanhos diferentes, mutation_function"
+    
+    #concatenate both arrays
+    solutions_rigidez = []
+    for i in range(len(ranked_rigidez)+len(crossover_rigidez)):
+        if i < len(ranked_rigidez):
+            solutions_rigidez.append(ranked_rigidez[i])
+        else:
+            solutions_rigidez.append(crossover_rigidez[i-len(ranked_rigidez)])
+    solutions_aco = []
+    for i in range(len(ranked_aco)+len(crossover_aco)):
+        if i < len(ranked_aco):
+            solutions_aco.append(ranked_aco[i])
+        else:
+            solutions_aco.append(crossover_aco[i-len(ranked_aco)])
+   
+   
+    for _ in range(generation_size-10):
+        one_rigidez = []
+        one_aco = []
+        #dado o vector concatenado solutions, o valor de i que é quem vai escolher o individuo a ser mutado para criar um novo, 
+        # será em média, 50% das vezes a primeira metade do vector (ranked_rigidez) que é quem tem as melhores soluções 
+        # da generação antiga, tendendo a escolher o melhor através da expovariete e 50% totalmente randomico dos que receberem crossover (cruzamento)
+        best_or_crossver = random.randint(0, 1)
+        if(best_or_crossver == 1):
+            i = expovariate(len(ranked_rigidez))#escolhendo uma das melhores amostrar (metodo antigo)
+        else:
+            i = random.randint(0, len(crossover_rigidez)-1)+len(ranked_rigidez)
+        #------------------------------------------------------------------------------------------
+        signal = [-1, 1]
         
         for j in range(len(solutions_aco[0])):
             new_lenght_aco = select_thickness(aco_options_mm)[0]
@@ -407,13 +597,33 @@ def mutation_function(elements, generation_size):
     return new_rigidez, new_aco, new_lenght, new_width
 
 
+
+def select_pressure(generation_tuples):
+    vector_error = []
+
+    best_individual_error = generation_tuples[0][0]
+    for individual in generation_tuples:
+        if(individual[0] != 0):
+            vector_error.append(individual[0])
+    average_error = np.sum(vector_error)/(len(vector_error))
+    return best_individual_error, average_error
+
 #--------------------------teste select_function-------------------------------------
-print("--------------------------teste selection_function-------------------------------------")
-rigidez, aco, comprimento, largura = generate_new_solution(500)
-#parameters         [L,C,tmola,Eepdm],  [L,C,taco], [comprimento],  [largura,  [Hz, Hz, Hz, Hz, Hz],   limit_generation,   generation_size
-selection_function  (rigidez,           aco,        comprimento,    largura,   frequency_target,       30000,                500)
+print("--------------------------Start Code-------------------------------------")
+rigidez, aco, comprimento, largura = generate_new_solution(generation_size_)
+#parameters                     [L,C,tmola,Eepdm],  [L,C,taco], [comprimento],  [largura,  [Hz, Hz, Hz, Hz, Hz],   limit_generation,   generation_size
+undone = selection_function  (  rigidez,            aco,        comprimento,    largura,   frequency_target,       limit_generation_,  generation_size_)
+
+if chart_on:
+    plt.savefig('final_sp_2_2_1.png', dpi=300)
 
 
-#realizar a variação do comprimento entre 40 e 60 centimetro
-#largura 45 e 55mm
-#borracha variar de de 2mm a 6mm, variando de 0.5 em 0.5
+if(undone):
+    print("Preparing to rerun")
+    # time.sleep(1)
+    # for _ in range(5):
+    #     print(".")
+    #     time.sleep(0.6)
+    # os.execv(sys.executable, ['python3'] + sys.argv)
+
+print("--------------------End--------------------")

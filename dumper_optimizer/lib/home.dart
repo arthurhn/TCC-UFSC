7efee249788221515be1789f0c9e2597eb2c9330
Luan <luan.alflen4@gmail.com>
2023-09-21 02:44:01 -0300

    v1.3

--- dumper_optimizer/lib/home.dart
+++ dumper_optimizer/lib/home.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter/scheduler.dart';
 import 'main.dart';
 import 'formPage.dart';
 import 'package:flutter/material.dart';
@@ -28,7 +29,7 @@ class HomePage extends StatefulWidget {
 }
 
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin{
   late Dumper dumper;
   _HomePageState(this.dumper);
   late Timer _timer;
@@ -42,10 +43,12 @@ class _HomePageState extends State<HomePage> {
   List<Individual> crossoversolutions = [];
   int algorithm_status = 0;
   double current_select_pressure=0.0;
+  late TabController tabController;
 
   @override
   void initState(){
     super.initState();
+    tabController=TabController(length: 3, vsync: this);
     _timer = Timer.periodic(const Duration(milliseconds: 50), (timer) {
 
       //inicio do algoritmo genetico
@@ -116,6 +119,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget build(BuildContext context) {
+
 
     var currentWidth = MediaQuery.of(context).size.width;
     var currentHeight = MediaQuery.of(context).size.height;
@@ -189,54 +193,199 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+            Column(children: [
+              Container(height: 0.7, color: Colors.white.withOpacity(0.3),),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(0.0),
-                    child: Text('Objetivo: ${dumper.freq}', style: TextStyle(color: Colors.white),),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(0.0),
-                    child: Text(rankedsolutions.isNotEmpty ? 'Melhor resultado: ${rankedsolutions[0].vector_ans}' : 'Melhor resultado: []', style: TextStyle(color: Colors.white),),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Text(rankedsolutions.isNotEmpty ? 'Erro: ${rankedsolutions[0].vector_errors}' : 'Erro: []', style: TextStyle(color: Colors.white),),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15.0),
-                    child: Text('Generations: $generation', style: TextStyle(color: Colors.white),),
-                  ),
-                  Column(children: [
-                    for(int i=0; i<3;i++)
-                      Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Text(rankedsolutions.length < 3 ? 'Vazio' : 'individual[$i] - accuracy: ${(rankedsolutions[i].accuracy).toStringAsFixed(2)}, weight: ${(rankedsolutions[i].weight).toStringAsFixed(2)} Kg, aco = [${(rankedsolutions[i].aco[0][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].aco[1][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].aco[2][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].aco[3][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].aco[4][2]*1000).toStringAsFixed(1)}], borracha = [${(rankedsolutions[i].rigidez[0][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].rigidez[1][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].rigidez[2][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].rigidez[3][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].rigidez[4][2]*1000).toStringAsFixed(1)}]', style: TextStyle(color: Colors.white),),
+                  SizedBox(width: 125,),
+                  Container(
+                    height: 26,
+                    child: TabBar(
+                      indicator: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: Colors.grey.withOpacity(0.15),
+
                       ),
-                  ],
+                      controller: tabController,
+                      isScrollable: true,
+                      labelPadding: const EdgeInsets.symmetric(horizontal: 30),
+                      tabs: const [
+                        Tab(child: SizedBox(width: 70, child: Center(child: Text("Principal", style: TextStyle(color: Color(0xff62b5e5),),))),),
+                        Tab(child: SizedBox(width: 70, child: Center(child: Text("Geral", style: TextStyle(color: Color(0xff62b5e5),),))),),
+                        Tab(child: SizedBox(width: 70, child: Center(child: Text("Resultados", style: TextStyle(color: Color(0xff62b5e5),),))),),
+                      ],
+                    ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 0.0),
-                    child: Text('Select_pressure: ${current_select_pressure.toStringAsFixed(3)}', style: TextStyle(color: Colors.white),),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15.0),
-                    child: Text('Tempo de execução: ${printTimeFromMicroseconds(currentExecutionTime)}', style: TextStyle(color: Colors.white),),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15.0),
-                    child: Text('Tempo de execução média de uma geração: ${(averageExecutionTime/1000).toStringAsFixed(0)}ms', style: TextStyle(color: Colors.white),),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15.0),
-                    child: Text('Status: $algorithm_status', style: TextStyle(color: Colors.white),),
-                  ),
+                  SizedBox(
+                    width: 125,
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 7),
+                          child: Icon(Icons.play_arrow, color: Colors.green,),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 7),
+                          child: Icon(Icons.pause, color: Colors.blue,),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 7),
+                          child: Icon(Icons.stop, color: Colors.red,),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
-            ),
+              Container(height: 0.7, color: Colors.white.withOpacity(0.3),),
+            ],),
+            Expanded(
+                child: TabBarView(
+                  controller: tabController,
+                  children: [
+                    ListView.builder(
+                      physics: BouncingScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 1,
+                      itemBuilder: (context, index){
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(height: 25,),
+                            Padding(
+                              padding: const EdgeInsets.all(0.0),
+                              child: Text('Objetivo: ${dumper.freq}', style: TextStyle(color: Colors.white),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(0.0),
+                              child: Text(rankedsolutions.isNotEmpty ? 'Melhor resultado: ${rankedsolutions[0].vector_ans}' : 'Melhor resultado: []', style: TextStyle(color: Colors.white),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text(rankedsolutions.isNotEmpty ? 'Erro: ${rankedsolutions[0].vector_errors}' : 'Erro: []', style: TextStyle(color: Colors.white),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 15.0),
+                              child: Text('Generations: $generation', style: TextStyle(color: Colors.white),),
+                            ),
+                            Column(children: [
+                              for(int i=0; i<3;i++)
+                                Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: Text(rankedsolutions.length < 3 ? 'Vazio' : 'individual[$i] - accuracy: ${(rankedsolutions[i].accuracy).toStringAsFixed(2)}, weight: ${(rankedsolutions[i].weight).toStringAsFixed(2)} Kg, aco = [${(rankedsolutions[i].aco[0][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].aco[1][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].aco[2][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].aco[3][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].aco[4][2]*1000).toStringAsFixed(1)}], borracha = [${(rankedsolutions[i].rigidez[0][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].rigidez[1][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].rigidez[2][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].rigidez[3][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].rigidez[4][2]*1000).toStringAsFixed(1)}]', style: TextStyle(color: Colors.white),),
+                                ),
+                            ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 0.0),
+                              child: Text('Select_pressure: ${current_select_pressure.toStringAsFixed(3)}', style: TextStyle(color: Colors.white),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 15.0),
+                              child: Text('Tempo de execução: ${printTimeFromMicroseconds(currentExecutionTime)}', style: TextStyle(color: Colors.white),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 15.0),
+                              child: Text('Tempo de execução média de uma geração: ${(averageExecutionTime/1000).toStringAsFixed(0)}ms', style: TextStyle(color: Colors.white),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 15.0),
+                              child: Text('Status: $algorithm_status', style: TextStyle(color: Colors.white),),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                    ListView.builder(
+                      physics: BouncingScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 1,
+                      itemBuilder: (context, index){
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: const [
+                            SizedBox(height: 25,),
+                            Padding(
+                              padding: EdgeInsets.all(0.0),
+                              child: Text('Under construction...', style: TextStyle(color: Colors.white),),
+                            ),
+
+                          ],
+                        );
+                      },
+                    ),
+                    ListView.builder(
+                      physics: BouncingScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 1,
+                      itemBuilder: (context, index){
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: const [
+                            SizedBox(height: 25,),
+                            Padding(
+                              padding: EdgeInsets.all(0.0),
+                              child: Text('Under construction....', style: TextStyle(color: Colors.white),),
+                            ),
+
+                          ],
+                        );
+                      },
+                    ),
+                  ],
+                ))
+            // Expanded(
+            //   child: Column(
+            //     mainAxisAlignment: MainAxisAlignment.center,
+            //     crossAxisAlignment: CrossAxisAlignment.center,
+            //     children: [
+            //       Padding(
+            //         padding: const EdgeInsets.all(0.0),
+            //         child: Text('Objetivo: ${dumper.freq}', style: TextStyle(color: Colors.white),),
+            //       ),
+            //       Padding(
+            //         padding: const EdgeInsets.all(0.0),
+            //         child: Text(rankedsolutions.isNotEmpty ? 'Melhor resultado: ${rankedsolutions[0].vector_ans}' : 'Melhor resultado: []', style: TextStyle(color: Colors.white),),
+            //       ),
+            //       Padding(
+            //         padding: const EdgeInsets.all(15.0),
+            //         child: Text(rankedsolutions.isNotEmpty ? 'Erro: ${rankedsolutions[0].vector_errors}' : 'Erro: []', style: TextStyle(color: Colors.white),),
+            //       ),
+            //       Padding(
+            //         padding: const EdgeInsets.only(top: 15.0),
+            //         child: Text('Generations: $generation', style: TextStyle(color: Colors.white),),
+            //       ),
+            //       Column(children: [
+            //         for(int i=0; i<3;i++)
+            //           Padding(
+            //             padding: const EdgeInsets.all(5.0),
+            //             child: Text(rankedsolutions.length < 3 ? 'Vazio' : 'individual[$i] - accuracy: ${(rankedsolutions[i].accuracy).toStringAsFixed(2)}, weight: ${(rankedsolutions[i].weight).toStringAsFixed(2)} Kg, aco = [${(rankedsolutions[i].aco[0][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].aco[1][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].aco[2][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].aco[3][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].aco[4][2]*1000).toStringAsFixed(1)}], borracha = [${(rankedsolutions[i].rigidez[0][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].rigidez[1][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].rigidez[2][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].rigidez[3][2]*1000).toStringAsFixed(1)}, ${(rankedsolutions[i].rigidez[4][2]*1000).toStringAsFixed(1)}]', style: TextStyle(color: Colors.white),),
+            //           ),
+            //       ],
+            //       ),
+            //       Padding(
+            //         padding: const EdgeInsets.only(top: 0.0),
+            //         child: Text('Select_pressure: ${current_select_pressure.toStringAsFixed(3)}', style: TextStyle(color: Colors.white),),
+            //       ),
+            //       Padding(
+            //         padding: const EdgeInsets.only(top: 15.0),
+            //         child: Text('Tempo de execução: ${printTimeFromMicroseconds(currentExecutionTime)}', style: TextStyle(color: Colors.white),),
+            //       ),
+            //       Padding(
+            //         padding: const EdgeInsets.only(top: 15.0),
+            //         child: Text('Tempo de execução média de uma geração: ${(averageExecutionTime/1000).toStringAsFixed(0)}ms', style: TextStyle(color: Colors.white),),
+            //       ),
+            //       Padding(
+            //         padding: const EdgeInsets.only(top: 15.0),
+            //         child: Text('Status: $algorithm_status', style: TextStyle(color: Colors.white),),
+            //       ),
+            //     ],
+            //   ),
+            // ),
           ]
         ),
       )
